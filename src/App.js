2900c299f3b8b01b53a8190006c6fcfde90a80fd
ledalla sandeep <ledallasandeepsandy@gmail.com>
2024-12-27 23:28:34 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,31 +1,32 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import Header from "./Components/Layout/Header";
 import Meals from "./Components/Meals/Meals";
-import 'bootstrap/dist/css/bootstrap.min.css'
+import "bootstrap/dist/css/bootstrap.min.css";
 import Cart from "./Components/Cart/Cart";
 import CartProvider from "./store/CartProvider";
+import Footer from "./Components/Footer";
 
 function App() {
+  const [cartIsShown, setcartIsShown] = useState(false);
 
-  const[cartIsShown,setcartIsShown]=useState(false)
+  const ShowCartHandler = (props) => {
+    setcartIsShown(true);
+  };
 
-  const ShowCartHandler=(props)=>{
-    setcartIsShown(true)
-  }
-
-  const HideCartHandler=()=>{
-    setcartIsShown(false)
-  }
+  const HideCartHandler = () => {
+    setcartIsShown(false);
+  };
 
   return (
     <CartProvider>
-       {cartIsShown && <Cart onClose={HideCartHandler} />}
-           <Header onShowCart={ShowCartHandler}/>
-            <main>
-              <Meals />
-            </main>
+      {cartIsShown && <Cart onClose={HideCartHandler} />}
+      <Header onShowCart={ShowCartHandler} />
+      <main>
+        <Meals />
+      </main>
+      <Footer />
     </CartProvider>
-  )
+  );
 }
 
 export default App;
---
+++ src/Components/Footer.js
@@ -0,0 +1,17 @@
+// import React from "react";
+// import { IoIosMail } from "react-icons/io";
+// import { MdCall } from "react-icons/md";
+
+// const Footer = () => {
+//   return (
+//     <div className="bg-black h-50 w-[100%]">
+//       <div>
+//         <IoIosMail />
+//         <MdCall />
+//       </div>
+//       <div></div>
+//     </div>
+//   );
+// };
+
+// export default Footer;
